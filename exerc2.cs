10dbd4c4822c654a6dc7bb80da478b6ab1baf78d
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-02-27 04:28:40 -0300

    Exercicio readLine method

---
+++ exerc2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace exerc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            string cidade;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+
+            Console.Write("Digite o nome da cidade que voce nasceu: ");
+            cidade = Console.ReadLine();
+
+
+            Console.WriteLine("\n*******Informações digitadas*******\n");
+            Console.WriteLine("Seu nome: " + nome);
+            Console.WriteLine("Cidade de nascimento: " + cidade);
+
+            Console.ReadKey();
+        }
+    }
+}
