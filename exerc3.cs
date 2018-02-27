4ade79d4ccf8d95710d0b6eb6839b50c7e86855e
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-02-27 04:35:21 -0300

    parse values

---
+++ exerc3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            double peso;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write(nome + ", Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.Write(nome + ", Digite seu peso: ");
+            peso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n********* Informações digitadas  *********");
+            Console.WriteLine("{0} tem {1} anos de idade e pesa {2:N3}kg", nome, idade, peso);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
