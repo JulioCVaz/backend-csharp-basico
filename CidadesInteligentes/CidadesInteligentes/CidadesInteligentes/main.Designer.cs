f6f44d7200b09d76f144344c91d672eff1a10cf2
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-09-03 21:58:11 -0300

    criado interface

--- CidadesInteligentes/CidadesInteligentes/CidadesInteligentes/main.Designer.cs
+++ CidadesInteligentes/CidadesInteligentes/CidadesInteligentes/main.Designer.cs
@@ -28,20 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.imgHome = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblSubTitulo = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // imgHome
+            // 
+            this.imgHome.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("imgHome.BackgroundImage")));
+            this.imgHome.Location = new System.Drawing.Point(618, -2);
+            this.imgHome.Name = "imgHome";
+            this.imgHome.Size = new System.Drawing.Size(592, 551);
+            this.imgHome.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblTitulo.Location = new System.Drawing.Point(164, 103);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(279, 33);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Cidades Inteligentes";
+            // 
+            // lblSubTitulo
+            // 
+            this.lblSubTitulo.AutoSize = true;
+            this.lblSubTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubTitulo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSubTitulo.Location = new System.Drawing.Point(244, 136);
+            this.lblSubTitulo.Name = "lblSubTitulo";
+            this.lblSubTitulo.Size = new System.Drawing.Size(114, 16);
+            this.lblSubTitulo.TabIndex = 2;
+            this.lblSubTitulo.Text = "Acesse seu futuro";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblLogin.Location = new System.Drawing.Point(106, 232);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(40, 13);
+            this.lblLogin.TabIndex = 3;
+            this.lblLogin.Text = "LOGIN";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblSenha.Location = new System.Drawing.Point(106, 311);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(44, 13);
+            this.lblSenha.TabIndex = 4;
+            this.lblSenha.Text = "SENHA";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(170, 232);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(273, 20);
+            this.txtLogin.TabIndex = 5;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(170, 308);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(273, 20);
+            this.txtSenha.TabIndex = 6;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnLogin.Location = new System.Drawing.Point(247, 370);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(111, 32);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "Logar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(1208, 545);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblSubTitulo);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.imgHome);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Name = "main";
+            this.Text = "Cidades Inteligentes";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel imgHome;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblSubTitulo;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
